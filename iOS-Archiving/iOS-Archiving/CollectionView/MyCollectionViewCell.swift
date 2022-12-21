df30131d6402d675d44b0fc4500312115d2a991b
kpk0616 <kpk0616@naver.com>
2022-12-21 18:29:33 +0900

    [Add] Add Snapkit package

---
+++ iOS-Archiving/iOS-Archiving/CollectionView/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  iOS-Archiving
+//
+//  Created by 박의서 on 2022/12/21.
+//
+
+import UIKit
+import Snapkit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+
+    
+}
