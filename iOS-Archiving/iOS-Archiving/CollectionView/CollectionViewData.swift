59ad78623780b2ffe62c0ea082e802cefaa931e3
kpk0616 <kpk0616@naver.com>
2022-12-21 18:20:05 +0900

    [Add] Add CollectionViewData

---
+++ iOS-Archiving/iOS-Archiving/CollectionView/CollectionViewData.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewData.swift
+//  iOS-Archiving
+//
+//  Created by 박의서 on 2022/12/21.
+//
+
+import Foundation
+
+struct CollectionViewData {
+    static let data = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+}
