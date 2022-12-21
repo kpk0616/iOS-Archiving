325a2163950e93210c4ff686b7c8b562fd4b4dda
kpk0616 <kpk0616@naver.com>
2022-12-21 18:33:02 +0900

    [Add] Add MyCollectionViewCell

--- iOS-Archiving/iOS-Archiving/CollectionView/MyCollectionViewCell.swift
+++ iOS-Archiving/iOS-Archiving/CollectionView/MyCollectionViewCell.swift
@@ -7,9 +7,36 @@
 
 import UIKit
 import SnapKit
+import Then
 
 class MyCollectionViewCell: UICollectionViewCell {
     
-
+    static var id: String {
+        return NSStringFromClass(Self.self).components(separatedBy: ".").last!
+    }
+    
+    var dataLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        dataLabel = UILabel()
+        dataLabel.font = UIFont.systemFont(ofSize: 32)
+        dataLabel.textColor = .label
+        dataLabel.textAlignment = .center
+        
+        contentView.addSubview(dataLabel)
+        dataLabel.snp.makeConstraints {
+            $0.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
     
 }
