333bf89d9fd93c291cb9929e535cfc42eb0612c7
kpk0616 <kpk0616@naver.com>
2022-12-21 18:21:04 +0900

    [Add] Add MyCollectionView

---
+++ iOS-Archiving/iOS-Archiving/CollectionView/MyCollectionView.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionView.swift
+//  iOS-Archiving
+//
+//  Created by 박의서 on 2022/12/21.
+//
+
+import Foundation
+
+class MyCollectionView: UICollectionView {
+    // Nothing todo
+}
