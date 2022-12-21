21952bc439e3b1b72869ad7b4b38e6095705d1f1
kpk0616 <kpk0616@naver.com>
2022-12-21 18:47:08 +0900

    [Feat] Set layout CollectionView

--- iOS-Archiving/iOS-Archiving/CollectionView/CollectionViewController.swift
+++ iOS-Archiving/iOS-Archiving/CollectionView/CollectionViewController.swift
@@ -14,6 +14,26 @@ final class CollectionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configure()
+    }
+}
+
+extension CollectionViewController {
+    private func configure() {
+        let collectionViewLayer = UICollectionViewFlowLayout()
+        collectionViewLayer.sectionInset = UIEdgeInsets(top: 5.0, left: 7.0, bottom: 5.0, right: 7.0)
+        collectionViewLayer.minimumLineSpacing = 5
+        collectionViewLayer.minimumInteritemSpacing = 1
+        
+        myCollectionView = MyCollectionView(frame: .zero, collectionViewLayout: collectionViewLayer)
+        myCollectionView.backgroundColor = .lightGray
+        view.addSubview(myCollectionView)
+        
+        myCollectionView.snp.makeConstraints {
+            $0.top.equalTo(120)
+            $0.left.right.bottom.equalToSuperview().inset(40)
+        }
     }
 }
 
