9869590ed1925b4d27218c66712961ac137d89de
kpk0616 <kpk0616@naver.com>
2022-12-27 18:32:46 +0900

    [Feat] Check SingleTon pattern printing

--- iOS-Archiving/iOS-Archiving/Singleton/SingleTonViewControllerB.swift
+++ iOS-Archiving/iOS-Archiving/Singleton/SingleTonViewControllerB.swift
@@ -9,15 +9,25 @@ import UIKit
 
 final class SingleTonViewControllerB: UIViewController {
   
-  let userInfo = UserInfo.shared
+  private let userInfo = UserInfo.shared
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setName()
+    setLayout()
   }
   
+
+}
+
+extension SingleTonViewControllerB {
   private func setName() {
     userInfo.name = "의서"
     print(userInfo.id)
-    print(userInfo.password)
+    print(userInfo.name)
+  }
+  
+  private func setLayout() {
+    view.backgroundColor = .white
   }
 }
