e45776d278139ec9bb21304166ba5a6df165c199
kpk0616 <kpk0616@naver.com>
2022-12-27 18:25:55 +0900

    [Feat] Feat navigation A to B

--- iOS-Archiving/iOS-Archiving/SceneDelegate.swift
+++ iOS-Archiving/iOS-Archiving/SceneDelegate.swift
@@ -13,9 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   
   
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let _ = (scene as? UIWindowScene) else { return }
     
     if let windowScene = scene as? UIWindowScene {
@@ -24,8 +21,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
       window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
       
       let rootVC = SingleTonViewControllerA()
+      let navigationController = UINavigationController(rootViewController: rootVC)
       
-      window.rootViewController = rootVC
+      window.rootViewController = navigationController
       window.makeKeyAndVisible()
       self.window = window
     }
--- iOS-Archiving/iOS-Archiving/Singleton/SingleTonViewControllerA.swift
+++ iOS-Archiving/iOS-Archiving/Singleton/SingleTonViewControllerA.swift
@@ -6,19 +6,44 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class SingleTonViewControllerA: UIViewController {
   
   let userInfo = UserInfo.shared
   
+  private lazy var navigationButton = UIButton().then {
+    $0.setTitle("다음 화면으로 이동", for: .normal)
+    $0.backgroundColor = .black
+    $0.addTarget(self, action: #selector(pushToB), for: .touchUpInside)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     setId()
+    setLayout()
   }
 }
 
 extension SingleTonViewControllerA {
   private func setId() {
     userInfo.id = "epark"
+    print(userInfo.id)
+  }
+  
+  private func setLayout() {
+    view.backgroundColor = .white
+    view.addSubview(navigationButton)
+    navigationButton.snp.makeConstraints {
+      $0.centerX.centerY.equalToSuperview()
+      $0.height.equalTo(50)
+      $0.width.equalTo(200)
+    }
+  }
+  
+  @objc private func pushToB() {
+    let singleTonB = SingleTonViewControllerB()
+    self.navigationController?.pushViewController(singleTonB, animated: true)
   }
 }
